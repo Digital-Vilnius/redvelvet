01e2bcf2806724a8a0b5029998979edba121d923
Vytautas Saulis <vaulis11@gmail.com>
2021-04-23 22:11:03 +0300

    Home page

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,19 @@
 import 'lightgallery.js/dist/js/lightgallery.min';
+
+import Swiper, {Autoplay, Pagination} from 'swiper';
+import 'swiper/swiper-bundle.css';
+
 import './scss/style.scss';
 
 lightGallery(document.getElementById('lightgallery'));
+
+Swiper.use([Autoplay, Pagination]);
+const swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
